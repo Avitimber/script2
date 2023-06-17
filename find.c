f9d182b33373960ebf43f1297f154b3e065f4ee4
Avitimber <102849684+Avitimber@users.noreply.github.com>
2023-06-17 13:43:37 +0800

    Update find.c

--- find.c
+++ find.c
@@ -2,34 +2,12 @@
 #include <stdlib.h>
 #include "coro.h"
 
-extern char *dir;
-extern void fs_walk();
-
 void usage()
 {
     puts("find -name file-name dir");
 }
 
-coro_t *coro_w;
-coro_t *coro_f;
-
-char *file_name;
-void find() {
-    
-}
-
 int main(int argc, char *argv[])
 {
-    if (argc != 4) {
-        usage();
-        return 0;
-    }
-    file_name = argv[2];
-    dir = argv[3];
-    puts(file_name);
-    puts(dir);
-    /* coro_w = coro_new(fs_walk);
-    coro_f = coro_new(find);
-    coro_boot(coro_f); */
-    return 0;
+    
 }
